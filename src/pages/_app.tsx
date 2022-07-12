9f01f8cda23044436bb26bdc98a2ab7383019496
Francis Lagares <francis.lagares@gmail.com>
2022-07-12 20:09:43 +0200

    feat: create Navbar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,15 @@ import 'src/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import Navbar from '../components/Navbar';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
